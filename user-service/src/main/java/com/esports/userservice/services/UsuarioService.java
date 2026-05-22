52e3fcd13ff0166dbe2fb58948000a150420a42f
Sopaipaaa <sopadetiburon21@gmail.com>
2026-05-22 01:53:36 -0400

    se a creado UsuarioService

---
+++ user-service/src/main/java/com/esports/userservice/services/UsuarioService.java
@@ -0,0 +1,16 @@
+package com.esports.userservice.services;
+
+import com.esports.userservice.models.Usuario;
+import com.esports.userservice.models.dtos.UsuarioDTO;
+import java.util.List;
+
+public interface UsuarioService {
+
+    List<Usuario> findAll();
+    List<Usuario> findByRol(Usuario.Rol rol);
+    List<Usuario> findByEstado(Usuario.Estado estado);
+    Usuario findById(Long id);
+    Usuario save(UsuarioDTO dto);
+    Usuario update(Long id, UsuarioDTO dto);
+    Usuario desactivar(Long id);
+}
