f4abc2491e0cfbd5374a1a92b412dff45fe40973
Sopaipaaa <sopadetiburon21@gmail.com>
2026-05-22 01:25:44 -0400

    se agrego UsuarioDTO

---
+++ user-service/src/main/java/com/esports/userservice/models/Usuario.java
@@ -0,0 +1,63 @@
+package com.esports.userservice.models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usuario_id")
+    private Long usuarioId;
+
+    @NotBlank(message = "El nombre no puede estar vacio")
+    @Column(nullable = false)
+    private String nombre;
+
+    // el nickname es unico por ejemplo: juanitoElPro777
+    @NotBlank(message = "El nickname no puede estar vacio")
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @NotBlank(message = "El email no puede estar vacio")
+    @Email(message = "El email debe tener formato valido")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // rol del usuario ejemplo JUGADOR, ORGANIZADOR, ADMINISTRADOR
+    @NotNull(message = "el rol es obligatorio")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
+
+    // el estado ejemplo: activo, inactivo, sancionado
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Estado estado = Estado.ACTIVO;
+
+    // roles que pueden tener el sistema
+    public enum Rol{
+        JUGADOR,
+        ORGANIZADOR,
+        ADMINISTRADOR
+    }
+
+    // posible estado del usuario
+    public enum Estado{
+        ACTIVO,
+        INACTIVO,
+        SANCIONADO
+    }
+
+}
---
+++ user-service/src/main/java/com/esports/userservice/models/dtos/UsuarioDTO.java
@@ -0,0 +1,25 @@
+package com.esports.userservice.models.dtos;
+
+import com.esports.userservice.models.Usuario;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+
+    @NotBlank(message = "El nombre no puede estar vacio")
+    private String nombre;
+
+    @NotBlank(message = "el nickname no puede estar vacio")
+    private String nickname;
+
+    @NotBlank(message = "El email no puede estar vacio")
+    @Email(message = "EL email debe tener formato valido")
+    private String email;
+
+    @NotNull(message = "El rol es obligatorio")
+    private Usuario.Rol rol;
+}
