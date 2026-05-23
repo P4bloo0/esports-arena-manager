60113c5dde1b55dcbcbd6f006ca74df48e70ae8b
Sopaipaaa <sopadetiburon21@gmail.com>
2026-05-23 19:23:47 -0400

    se ha agregado Torneo.java

--- team-service/src/main/java/com/esports/teamservice/controllers/ApiExceptionHandler.java
+++ team-service/src/main/java/com/esports/teamservice/controllers/ApiExceptionHandler.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
-// captura todos los errores y los devuelve como JSON legible
 @RestControllerAdvice
 public class ApiExceptionHandler {
-
-    // captura errores de validacion como @NotBlank, @NotNull
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -23,8 +20,6 @@ public class ApiExceptionHandler {
         }
         return ResponseEntity.badRequest().body(errors);
     }
-
-    // captura nuestra excepcion personalizada EquipoException
     @ExceptionHandler(EquipoException.class)
     public ResponseEntity<?> handleEquipoException(EquipoException ex) {
         Map<String, String> error = new HashMap<>();
