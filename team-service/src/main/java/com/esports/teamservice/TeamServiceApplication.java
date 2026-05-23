3cd69fefecbe5c0f89c8acd224e21a039d817600
Sopaipaaa <sopadetiburon21@gmail.com>
2026-05-23 00:20:24 -0400

    se a creado el Equipo.java de la carpeta models de team-service

--- team-service/src/main/java/com/esports/teamservice/TeamServiceApplication.java
+++ team-service/src/main/java/com/esports/teamservice/TeamServiceApplication.java
@@ -2,8 +2,9 @@ package com.esports.teamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableFeignClients
 public class TeamServiceApplication {
 
     public static void main(String[] args) {
