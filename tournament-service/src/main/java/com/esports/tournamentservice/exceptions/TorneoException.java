8ff80a8d12bb3fc83752d3a7c7c2464a05b33965
Sopaipaaa <sopadetiburon21@gmail.com>
2026-05-23 20:25:23 -0400

    JuegoDTO y TorneoException Terminados

---
+++ tournament-service/src/main/java/com/esports/tournamentservice/exceptions/TorneoException.java
@@ -0,0 +1,7 @@
+package com.esports.tournamentservice.exceptions;
+
+public class TorneoException extends RuntimeException{
+    public TorneoException(String message){
+        super(message);
+    }
+}
--- tournament-service/src/main/java/com/esports/tournamentservice/models/dtos/JuegoDTO.java
+++ tournament-service/src/main/java/com/esports/tournamentservice/models/dtos/JuegoDTO.java
@@ -6,8 +6,12 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @ToString
-public class JuegoDTO {
+public class JuegoDTO { //esto representa la info del juego que viene del game-service
 
     private Long juegoId;
     private String nombre;
+    private String genero;
+    private String modalidad;
+    private Integer jugadoresPorEquipo;
+    private Boolean estado;
 }
