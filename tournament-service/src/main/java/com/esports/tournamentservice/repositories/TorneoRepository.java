7658123bdb5345f37b2951bd2dbf835351829776
Sopaipaaa <sopadetiburon21@gmail.com>
2026-05-23 20:44:01 -0400

    Se han agregado TorneoRepository, JuegoClient y TorneoService en tournament-service

---
+++ tournament-service/src/main/java/com/esports/tournamentservice/repositories/TorneoRepository.java
@@ -0,0 +1,14 @@
+package com.esports.tournamentservice.repositories;
+
+import com.esports.tournamentservice.models.Torneo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TorneoRepository extends JpaRepository<Torneo, Long> {
+
+    List<Torneo> findByEstado(String estado);
+
+    List<Torneo> findByJuegoId(Long juegoId);
+}
---
+++ tournament-service/src/main/java/com/esports/tournamentservice/services/TorneoService.java
@@ -0,0 +1,17 @@
+package com.esports.tournamentservice.services;
+
+import com.esports.tournamentservice.models.Torneo;
+import com.esports.tournamentservice.models.dtos.TorneoDTO;
+import java.util.List;
+
+public interface TorneoService {
+
+    List<Torneo> finAll();
+    Torneo findById(Long id);
+    List<Torneo> findByEstado(String estado);
+    List<Torneo> findByJuegoId(Long juegoId);
+    Torneo save(TorneoDTO dto);
+    Torneo update(Long id, TorneoDTO dto);
+    Torneo cambiarEstado(Long id, String nuevoEstado);
+
+}
